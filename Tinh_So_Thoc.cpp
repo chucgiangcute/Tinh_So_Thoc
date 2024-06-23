2aa6ab7ddd683e60795fd84029bc45ad0a5287e3
Giang Chúc <chucgiang218@gmail.com>
2024-06-23 10:20:04 +0700

    Add project files.

---
+++ Tinh_So_Thoc.cpp
@@ -0,0 +1,16 @@
+// Tinh_So_Thoc.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    double tonghatthoc = 0;
+
+    for (double i = 0; i < 64; i++) {
+        tonghatthoc += pow(2, i);
+    }
+
+    cout << "Tong so thoc tren ban co: " << tonghatthoc << endl;
+
+    return 0;
+}
